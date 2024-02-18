9aad0daf31c0b34293d90da61770a6e9fbc6d824
Yurui-Feng <yurifeng@bu.edu>
2024-02-18 15:52:15 -0800

    added http listner

--- backend/backend-service.tf
+++ backend/backend-service.tf
@@ -76,6 +76,13 @@ resource "aws_security_group" "alb_sg" {
     vpc_id = data.aws_vpc.default.id
 
     ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        }
+
+    ingress {
         from_port = 443
         to_port = 443
         protocol = "tcp"
